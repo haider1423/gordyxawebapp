818d72bff4319fab850848a303741098f152e1cd
haider1423 <51236525+haider1423@users.noreply.github.com>
2026-08-18 09:36:20 +0500

    installer error fixing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $directories = [
+            storage_path('framework/views'),
+            storage_path('framework/cache'),
+            storage_path('framework/cache/data'),
+            storage_path('framework/sessions'),
+            storage_path('framework/testing'),
+            storage_path('logs'),
+            base_path('bootstrap/cache'),
+        ];
+
+        foreach ($directories as $dir) {
+            if (!is_dir($dir)) {
+                @mkdir($dir, 0775, true);
+            }
+        }
+
+        if (!config('view.compiled')) {
+            config(['view.compiled' => storage_path('framework/views')]);
+        }
     }
 
     /**
