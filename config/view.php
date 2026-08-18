80e33afbc9316e17f3f3c432f3928d6b09d95d71
haider1423 <51236525+haider1423@users.noreply.github.com>
2026-08-18 09:50:44 +0500

    installer error fixing

--- config/view.php
+++ config/view.php
@@ -28,9 +28,6 @@ return [
     |
     */
 
-    'compiled' => env(
-        'VIEW_COMPILED_PATH',
-        storage_path('framework/views')
-    ),
+    'compiled' => storage_path('framework/views'),
 
 ];
